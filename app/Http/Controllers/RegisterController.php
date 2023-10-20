86e7f39fa764e97d5c31360ff5ad809a1bc247fb
KorolEzhey <yegortrukan@mail.ru>
2023-10-20 16:03:15 +0600

    Core ili fundament

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Person;
+
+class RegisterController extends Controller
+{
+    public function index(Request $request) {
+        // Создайте новую запись
+        // $person = new Person;
+        // $person->name = 'KorolEzhey'; // Замените на реальное имя
+        // $person->email = 'yegortrukan@mail.ru'; // Замените на реальный email
+        // $person->password = bcrypt('1245'); // Замените на реальный пароль
+        // $person->passport = '88005553535'; // Замените на реальный номер паспорта
+
+        // Сохраните запись в базе данных
+       // $person->save();
+
+        $person = Person::where('name', 'KorolEzhey')->first();
+        $personName = $person->name;
+
+        auth()->login($person);
+        return redirect('/');
+
+        // echo $person;
+
+        // return view('register');
+    }
+}
